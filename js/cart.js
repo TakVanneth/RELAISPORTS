17304b5ac7940619756bbeed0a7a1d017bdad26e
Vanneth <vanneth@Tak-Vanneth.local>
2023-11-12 23:58:21 +0700

    edit account and cart page

--- js/cart.js
+++ js/cart.js
@@ -40,8 +40,8 @@ $(document).ready(function() {
               <h6 class="text-black mb-0">${'$'+card.productprice}</h6>
             </div>
             <div class="col-md-1 col-lg-1 col-xl-1">
-              <a href="#" class="btn btn-primary remove_this_product" data-rowid="${index}">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+              <a href="#" class="btn remove_this_product" data-rowid="${index}" style="color:red">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
                   <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
                 </svg>
               </a>
@@ -130,9 +130,8 @@ $(document).ready(function() {
   });
 
   const itemCount = cardData ? cardData.length : 0;
-  $(".itemCountDisplay").text(itemCount + ' items');
-
-
+  $(".itemCountDisplay").text(itemCount === 0 ? 'No items' : itemCount === 1 ? '1 item' : itemCount + ' items');
+  
     // Function to update the #shipping element and recalculate totals
     function updateShippingAndTotal() {
       const selectedValue = parseFloat($("#shippingOption").val());
